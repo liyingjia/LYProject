3805bf5fe914b9fd2c30e37195ff4e5689e5f6c4
liying <liying@lcworld-inc.com>
2018-09-26 16:46:44 +0800

    添加一些baseview

---
+++ LYProject/BaseView/BaseModel.m
@@ -0,0 +1,13 @@
+//
+//  BaseModel.m
+//  LYProject
+//
+//  Created by 李营 on 2018/9/26.
+//  Copyright © 2018年 liying. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@implementation BaseModel
+
+@end
---
+++ LYProject/BaseView/BaseTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableViewCell.h
+//  LYProject
+//
+//  Created by 李营 on 2018/9/26.
+//  Copyright © 2018年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTableViewCell : UITableViewCell
+
+@end
